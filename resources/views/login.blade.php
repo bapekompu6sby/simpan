14ec62d07220b13c0983598d484ed1319e019304
Uhycore <arilmubin0@gmail.com>
2025-09-24 10:42:59 +0700

    test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-    <title>Simpan - Login Page</title>
+    <title>Simpan - Login Page aril</title>
 
     <meta name="description"
         content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
